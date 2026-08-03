c2d4faa7f455ca9e5c467184f54d5e76f1c7b50f
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 18:28:29 +0300

    Update UART_config.h

--- FIRMWARE/MCAL/UART/UART_config.h
+++ FIRMWARE/MCAL/UART/UART_config.h
@@ -1,3 +1,18 @@
+/**
+ * @file      UART_config.h
+ * @author    Moaaz
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Configuration file for the UART Driver.
+ * @details   This file is reserved for user-configurable parameters for the UART module.
+ *            Currently, the UART driver relies on dynamic runtime configuration via 
+ *            the initialization API (UART_Init) and the configuration structure. 
+ *            Pre-build static configurations can be added here if needed in the future.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
+
+
 #ifndef _UART_CONFIG_H_
 #define _UART_CONFIG_H_
 
