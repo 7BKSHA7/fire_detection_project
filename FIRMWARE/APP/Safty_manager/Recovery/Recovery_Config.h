b683bca209c68615cc3780507687319585d05597
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 20:48:29 +0300

    Update Recovery_Config.h

--- FIRMWARE/APP/Safty_manager/Recovery/Recovery_Config.h
+++ FIRMWARE/APP/Safty_manager/Recovery/Recovery_Config.h
@@ -1,14 +1,16 @@
 /**
-* @file    Recovery_Config.h
-* @author (developer)  
-* @author (reviewer)
-* @brief  
-* @details
-* @version
-* @date
-* @copyright Copyright (c) 2026, Gestell Company
-*/
-
+ * @file      Recovery_Config.h
+ * @author    Beshoy
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Configuration file for the Recovery subsystem.
+ * @details   This file is reserved for user-configurable parameters related to 
+ *            the recovery and acknowledgment process (e.g., timeout durations, 
+ *            UI message configurations). Currently empty but maintained for 
+ *            architectural consistency.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
 #ifndef RECOVERY_CONFIG_H
 #define RECOVERY_CONFIG_H
 
