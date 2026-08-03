67d1762b8d3cd3c2b771c2760f9abcb6d9c98541
Ali Farouk <aalimmohamed031@gmail.com>
2026-08-03 21:36:09 +0300

    Update Warning_Config.h

--- FIRMWARE/APP/Fire_detection/Warning/Warning_Config.h
+++ FIRMWARE/APP/Fire_detection/Warning/Warning_Config.h
@@ -1,27 +1,22 @@
 /**
- * @file      Warning_Config.h
- * @author    Ali
- * @author    Hesham Ahmed (Reviewer)
- * @brief     Configuration file for the Warning Fire State.
- * @details   This file contains user-configurable thresholds specific to 
- *            the Warning state logic, defining the lower and upper limits 
- *            for temperature and smoke to handle escalation and de-escalation.
- * @version   1.0.0
- * @date      3 August 2026
- * @copyright Copyright (c) 2026, Gestell Company
- */
+* @file    Warning_Config.h
+* @author (developer)  
+* @author (reviewer)
+* @brief  
+* @details
+* @version
+* @date
+* @copyright Copyright (c) 2026, Gestell Company
+*/
 
 #ifndef WARNING_CONFIG_H_
 #define WARNING_CONFIG_H_
 
 #include "../../../Lib/STD_TYPES.h"
 
-/* Warning state range: 60 > temp >= 45  OR  170 > smoke >= 70 */
-#define WARNING_TEMP_LOW        45   // >= this enters/stays in warning
-#define WARNING_TEMP_HIGH       60   // >= this escalates to fire
+#define WARNING_TEMP_THRESHOLD      60   // >= this escalates to fire
 
-#define WARNING_SMOKE_LOW       70   // >= this enters/stays in warning
-#define WARNING_SMOKE_HIGH      170  // >= this escalates to fire
+#define WARNING_SMOKE_THRESHOLD      170  // >= this escalates to fire
 
 #endif /* WARNING_CONFIG_H_ */
 
