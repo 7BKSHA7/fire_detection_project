686bff25d397a71200069b10c7e6be8296dab392
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 21:13:58 +0300

    Update header comments in Emergency_Private.h

--- FIRMWARE/APP/Fire_detection/Emergency/Emergency_Private.h
+++ FIRMWARE/APP/Fire_detection/Emergency/Emergency_Private.h
@@ -1,13 +1,15 @@
 /**
-* @file    Emergency_Private.h
-* @author (developer)  
-* @author (reviewer)
-* @brief  
-* @details
-* @version
-* @date
-* @copyright Copyright (c) 2026, Gestell Company
-*/
+ * @file      Emergency_Private.h
+ * @author    Ali
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Private definitions for the Emergency Fire State.
+ * @details   This file contains internal macros, threshold values, and private 
+ *            definitions specific to the Emergency state logic.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
+
 
 #ifndef EMERGENCY_PRIVATE_H
 #define EMERGENCY_PRIVATE_H
