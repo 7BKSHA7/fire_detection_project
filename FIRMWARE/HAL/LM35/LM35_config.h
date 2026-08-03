54f364496229f735d85b22ea7ae108bcb44a45d4
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 11:28:55 +0300

    Enhance LM35_config.h with documentation and comments
    Updated the LM35 configuration file to include detailed documentation and maintain existing configurations.

--- FIRMWARE/HAL/LM35/LM35_config.h
+++ FIRMWARE/HAL/LM35/LM35_config.h
@@ -1,3 +1,16 @@
+/**
+ * @file      LM35_config.h
+ * @author    Moaaz
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Configuration file for the LM35 Temperature Sensor Driver.
+ * @details   This file contains user-configurable parameters such as the hardware 
+ *            port, pin, and the specific ADC channel to which the LM35 sensor is connected.
+ *            Users can easily migrate the sensor to another ADC pin by updating these macros.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
+
 #ifndef _LM35_CONFIG_H_
 #define _LM35_CONFIG_H_
 
