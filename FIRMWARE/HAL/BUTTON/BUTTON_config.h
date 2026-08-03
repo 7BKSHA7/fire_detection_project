b13a46878bbf8944dde543afd86b4190e5c2d378
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 11:04:59 +0300

    Revise documentation in BUTTON_config.h
    Updated author and reviewer information, added brief and details about the button driver configuration.

--- FIRMWARE/HAL/BUTTON/BUTTON_config.h
+++ FIRMWARE/HAL/BUTTON/BUTTON_config.h
@@ -1,13 +1,15 @@
 /**
-* @file    Button_Config.h
-* @author (developer)  
-* @author (reviewer)
-* @brief  
-* @details
-* @version
-* @date
-* @copyright Copyright (c) 2026, Gestell Company
-*/
+ * @file      Button_Config.h
+ * @author    Youssef
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Configuration file for the Button Driver.
+ * @details   This file contains user-configurable parameters such as the hardware 
+ *            port and pin to which the button is connected. Users can modify 
+ *            these values based on the hardware schematic.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
 
 #ifndef BUTTON_CONFIG_H
 #define BUTTON_CONFIG_H
