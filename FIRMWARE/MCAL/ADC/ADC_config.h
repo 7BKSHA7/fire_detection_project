b0cc1c25c06d6e41dd059985bce7d0d0c560e08f
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 17:43:16 +0300

    Enhance ADC_config.h with documentation and metadata
    Added detailed documentation for the ADC configuration file, including authorship and versioning information.

--- FIRMWARE/MCAL/ADC/ADC_config.h
+++ FIRMWARE/MCAL/ADC/ADC_config.h
@@ -1,3 +1,18 @@
+/**
+ * @file      ADC_config.h
+ * @author    Rohayem
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Configuration file for the ADC Driver.
+ * @details   This file is reserved for user-configurable parameters for the ADC module.
+ *            Currently, the ADC driver relies on dynamic runtime configuration via 
+ *            the initialization and control APIs (e.g., ADC_Init, ADC_Prescaler). 
+ *            Static configurations (like timeout limits for polling) can be added here if needed.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
+
+
 #ifndef ADC_CONFIG_H_
 #define ADC_CONFIG_H_
 
