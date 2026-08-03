f726d196b8bf820659c0564831160f23c7777307
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 18:14:20 +0300

    Update TIMER_config.h

--- FIRMWARE/MCAL/TIMER/TIMER_config.h
+++ FIRMWARE/MCAL/TIMER/TIMER_config.h
@@ -1,3 +1,18 @@
+/**
+ * @file      TIMER_config.h
+ * @author    Beshoy
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Configuration file for the TIMER0 Driver.
+ * @details   This file is reserved for user-configurable parameters for the TIMER module.
+ *            Currently, the TIMER driver relies on dynamic runtime configuration via 
+ *            the initialization API (TIMER0_init) and the configuration structure. 
+ *            Pre-build static configurations can be added here if needed in the future.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
+
+
 #ifndef _TIMER_CONFIG_H_
 #define _TIMER_CONFIG_H_
 
