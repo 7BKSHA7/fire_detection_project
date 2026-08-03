bce47e7cf0c485e626d1aa65c9c7a8441493065d
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 11:02:48 +0300

    Update header comments in BUTTON_private.h

--- FIRMWARE/HAL/BUTTON/BUTTON_private.h
+++ FIRMWARE/HAL/BUTTON/BUTTON_private.h
@@ -1,13 +1,14 @@
 /**
-* @file    Button_Private.h
-* @author (developer)  
-* @author (reviewer)
-* @brief  
-* @details
-* @version
-* @date
-* @copyright Copyright (c) 2026, Gestell Company
-*/
+ * @file      Button_Private.h
+ * @author    Youssef
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Private definitions for the Button Driver.
+ * @details   This file contains private macros and state definitions (Pressed/Released) 
+ *            used internally by the Button driver implementation.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
 
 #ifndef BUTTON_PRIVATE_H
 #define BUTTON_PRIVATE_H
