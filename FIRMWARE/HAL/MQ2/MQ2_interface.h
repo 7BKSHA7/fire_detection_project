9c0884cbf697d18bfebe05627bcf306b887c0c0a
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 11:42:20 +0300

    Update MQ2_interface.h

--- FIRMWARE/HAL/MQ2/MQ2_interface.h
+++ FIRMWARE/HAL/MQ2/MQ2_interface.h
@@ -1,13 +1,14 @@
 /**
-* @file    MQ2_Interface.h
-* @author (developer)  
-* @author (reviewer)
-* @brief  
-* @details
-* @version
-* @date
-* @copyright Copyright (c) 2026, Gestell Company
-*/
+ * @file      MQ2_Interface.h
+ * @author    Moaaz
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Interface of the MQ2 Smoke/Gas Sensor Driver.
+ * @details   This file contains the function prototypes and public APIs used by 
+ *            upper layers to initialize the MQ2 sensor and read the smoke concentration data.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
 
 #ifndef MQ2_INTERFACE_H
 #define MQ2_INTERFACE_H
