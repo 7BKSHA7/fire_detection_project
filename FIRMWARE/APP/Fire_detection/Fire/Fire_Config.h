9d5ac3c5eadc548f44ea8a703d635062db261d62
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 21:30:42 +0300

    Update documentation in Fire_Config.h

--- FIRMWARE/APP/Fire_detection/Fire/Fire_Config.h
+++ FIRMWARE/APP/Fire_detection/Fire/Fire_Config.h
@@ -1,13 +1,18 @@
 /**
-* @file    Fire_Config.h
-* @author (developer)  
-* @author (reviewer)
-* @brief  
-* @details
-* @version
-* @date
-* @copyright Copyright (c) 2026, Gestell Company
-*/
+ * @file      Fire_Config.h
+ * @author    Beshoy
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Configuration file for the Fire State.
+ * @details   This file contains user-configurable thresholds specific to 
+ *            the Fire state logic, defining the lower and upper limits 
+ *            for temperature and smoke to handle escalation to emergency 
+ *            or de-escalation back to warning.
+ *            
+ *            Fire state range: 70 > temp >= 60  OR  200 > smoke >= 130
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
 
 #ifndef FIRE_CONFIG_H
 #define FIRE_CONFIG_H
