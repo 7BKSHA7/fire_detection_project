bfbaa0c095361728a10727753db63ab25939b775
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 21:14:53 +0300

    Update Emergency_Interface.h

--- FIRMWARE/APP/Fire_detection/Emergency/Emergency_Interface.h
+++ FIRMWARE/APP/Fire_detection/Emergency/Emergency_Interface.h
@@ -1,13 +1,17 @@
 /**
-* @file    Emergency_Interface.h
-* @author (developer)  
-* @author (reviewer)
-* @brief  
-* @details
-* @version
-* @date
-* @copyright Copyright (c) 2026, Gestell Company
-*/
+ * @file      Emergency_Interface.h
+ * @author    Beshoy
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Interface for the Emergency Fire State.
+ * @details   This file contains the function prototypes and public APIs 
+ *            for the Emergency state. Based on system requirements:
+ *            - Triggered when Temp > 70 & Smoke > 200.
+ *            - Actions: Red light blinks, Buzzer toggles, LCD shows warning.
+ *            - Sensor readings stop until manual ACK is received.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
 
 #ifndef EMERGENCY_INTERFACE_H
 #define EMERGENCY_INTERFACE_H
