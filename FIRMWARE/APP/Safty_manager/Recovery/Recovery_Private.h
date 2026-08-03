3e938c0df2b5d76bb865d139559fbd2442d9625a
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 20:46:35 +0300

    Update Recovery_Private.h

--- FIRMWARE/APP/Safty_manager/Recovery/Recovery_Private.h
+++ FIRMWARE/APP/Safty_manager/Recovery/Recovery_Private.h
@@ -1,13 +1,15 @@
 /**
-* @file    Recovery_Private.h
-* @author (developer)  
-* @author (reviewer)
-* @brief  
-* @details
-* @version
-* @date
-* @copyright Copyright (c) 2026, Gestell Company
-*/
+ * @file      Recovery_Private.h
+ * @author    Beshoy
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Private definitions for the Recovery subsystem.
+ * @details   This file is reserved for internal macros, states, or variables 
+ *            specific to the Recovery module. Currently, it is empty but 
+ *            maintained for architectural consistency.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
 
 #ifndef RECOVERY_PRIVATE_H
 #define RECOVERY_PRIVATE_H
