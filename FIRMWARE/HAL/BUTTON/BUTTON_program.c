2d8c2fa2197121dd0ef89c7b731a53b63f2ede38
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 10:43:32 +0300

    Enhance documentation in BUTTON_program.c
    Updated file documentation and added function comments for BUTTON_init and BUTTON_GetState.

--- FIRMWARE/HAL/BUTTON/BUTTON_program.c
+++ FIRMWARE/HAL/BUTTON/BUTTON_program.c
@@ -1,13 +1,14 @@
 /**
-* @file    Button_Program.c
-* @author (developer)  
-* @author (reviewer)
-* @brief  
-* @details
-* @version
-* @date
-* @copyright Copyright (c) 2026, Gestell Company
-*/
+ * @file      Button_Program.c
+ * @author    Youssef
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Implementation of the Button Driver.
+ * @details   This file contains the initialization function for the button (enabling internal pull-up) 
+ *            and the function to read the current state of the button (Pressed / Released).
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
 
 #include "../../LIB/STD_TYPES.h"
 #include "../../LIB/BIT_MATH.h"
@@ -17,12 +18,25 @@
 #include "BUTTON_interface.h"
 #include "../../MCAL/DIO/DIO_interface.h"
 
+/**
+ * @brief  Initializes the button pin.
+ * @details Sets the button pin as an input and activates the internal pull-up resistor.
+ * @param  void
+ * @return void
+ */
 void BUTTON_init(void)
 {
     DIO_set_pin_direction(BUTTON_PORT, BUTTON_PIN, input);
     DIO_set_pin_value(BUTTON_PORT, BUTTON_PIN, high); 
 }
 
+/**
+ * @brief  Gets the current state of the button.
+ * @details Reads the digital value of the button pin to determine if it is pressed or released 
+ *          (Active Low configuration).
+ * @param  void
+ * @return u8 Returns BUTTON_PRESSED if the button is pressed, or BUTTON_RELEASED if it is released.
+ */
 u8 BUTTON_GetState(void)
 {
     u8 pinLevel = DIO_get_pin_value(BUTTON_PORT, BUTTON_PIN);
