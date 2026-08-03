5b20b0d684c66053d4c43ffdaa8ce0b27c13fa08
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 11:28:05 +0300

    Enhance LM35_interface.h with detailed comments
    Added file documentation for LM35 interface header.

--- FIRMWARE/HAL/LM35/LM35_interface.h
+++ FIRMWARE/HAL/LM35/LM35_interface.h
@@ -1,3 +1,15 @@
+/**
+ * @file      LM35_interface.h
+ * @author    Moaaz
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Interface of the LM35 Temperature Sensor Driver.
+ * @details   This file contains the function prototypes and public APIs used by 
+ *            upper layers to initialize the LM35 sensor and read the temperature data.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
+
 #ifndef _LM35_INTERFACE_H_
 #define _LM35_INTERFACE_H_
 
