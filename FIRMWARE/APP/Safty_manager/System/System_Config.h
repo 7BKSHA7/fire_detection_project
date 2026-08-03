4977a725f64f45a2eee720610b02ad79fb2c69db
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 20:44:36 +0300

    Update header comments in System_Config.h

--- FIRMWARE/APP/Safty_manager/System/System_Config.h
+++ FIRMWARE/APP/Safty_manager/System/System_Config.h
@@ -1,13 +1,15 @@
 /**
-* @file    System_Config.h
-* @author (developer)  
-* @author (reviewer)
-* @brief  
-* @details
-* @version
-* @date
-* @copyright Copyright (c) 2026, Gestell Company
-*/
+ * @file      System_Config.h
+ * @author    Beshoy
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Configuration file for the core System application layer.
+ * @details   This file is reserved for system-wide configuration parameters 
+ *            such as temperature and smoke thresholds for fire detection, 
+ *            system timeout durations, and other application-level constants.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
 
 #ifndef _SYSTEM_CONFIG_H_
 #define _SYSTEM_CONFIG_H_
