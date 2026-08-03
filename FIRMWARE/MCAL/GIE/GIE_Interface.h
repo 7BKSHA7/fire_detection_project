01160ea1d7d0c6993d1a98fb9b45e9635277dc59
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 18:09:28 +0300

    Add documentation header to GIE_Interface.h
    Added file header documentation for GIE_Interface.h

--- FIRMWARE/MCAL/GIE/GIE_Interface.h
+++ FIRMWARE/MCAL/GIE/GIE_Interface.h
@@ -1,3 +1,15 @@
+/**
+ * @file      GIE_Interface.h
+ * @author    Beshoy
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Interface of the GIE (Global Interrupt Enable) Driver.
+ * @details   This file contains the function prototypes and public APIs used by 
+ *            upper layers to enable or disable global interrupts in the system.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
+
 
 #ifndef _GIE_INTERFACE_H
 #define _GIE_INTERFACE_H
