f0d443d2d83b74d6f474252474c85291bf3c8f2d
Ali Farouk <aalimmohamed031@gmail.com>
2026-08-03 21:54:13 +0300

    Update WARNING_voidRun return type to u8

--- FIRMWARE/APP/Fire_detection/Warning/Warning_Interface.h
+++ FIRMWARE/APP/Fire_detection/Warning/Warning_Interface.h
@@ -1,24 +1,11 @@
-/**
- * @file      Warning_Interface.h
- * @author    Ali
- * @author    Hesham Ahmed (Reviewer)
- * @brief     Interface for the Warning Fire State.
- * @details   This file contains the function prototypes and public APIs 
- *            for the Warning state. It includes the Normal interface to 
- *            share the global FIRE_STATE_t enumeration.
- * @version   1.0.0
- * @date      3 August 2026
- * @copyright Copyright (c) 2026, Gestell Company
- */
-
 #ifndef WARNING_INTERFACE_H_
 #define WARNING_INTERFACE_H_
 
-#include "../NORMAL/NORMAL_interface.h"   // FIRE_STATE_t shared definition
+#include "../NORMAL/NORMAL_interface.h"   // shared FIRE_STATE_* defines
 
 /* runs the warning-state behavior once, returns the state
  * the system should be in on the NEXT cycle
  */
-FIRE_STATE_t WARNING_voidRun(void);
+u8 WARNING_voidRun(void);
 
 #endif /* WARNING_INTERFACE_H_ */
