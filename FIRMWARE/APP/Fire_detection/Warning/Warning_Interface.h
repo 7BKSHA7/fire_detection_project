e69d509e148fcd4d9c0167534d44bf3c02424030
Ali Farouk <aalimmohamed031@gmail.com>
2026-08-03 21:07:38 +0300

    Update header guard and include statement in Warning_Interface.h

--- FIRMWARE/APP/Fire_detection/Warning/Warning_Interface.h
+++ FIRMWARE/APP/Fire_detection/Warning/Warning_Interface.h
@@ -9,9 +9,14 @@
 * @copyright Copyright (c) 2026, Gestell Company
 */
 
-#ifndef WARNING_INTERFACE_H
-#define WARNING_INTERFACE_H
+#ifndef WARNING_INTERFACE_H_
+#define WARNING_INTERFACE_H_
 
+#include "../NORMAL/NORMAL_interface.h"   // FIRE_STATE_t shared definition
 
+/* runs the warning-state behavior once, returns the state
+ * the system should be in on the NEXT cycle
+ */
+FIRE_STATE_t WARNING_voidRun(void);
 
-#endif /* WARNING_INTERFACE_H */
+#endif /* WARNING_INTERFACE_H_ */
