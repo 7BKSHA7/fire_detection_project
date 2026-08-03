e057a7acac0c8faf5cbf2b48df3783139b4b3acc
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 18:02:48 +0300

    Enhance EXTI_config.h with documentation comments
    Added detailed documentation for the EXTI configuration file.

--- FIRMWARE/MCAL/EXTI/EXTI_config.h
+++ FIRMWARE/MCAL/EXTI/EXTI_config.h
@@ -1,3 +1,18 @@
+/**
+ * @file      EXTI_config.h
+ * @author    Ali
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Configuration file for the EXTI (External Interrupt) Driver.
+ * @details   This file is reserved for user-configurable parameters for the EXTI module.
+ *            Currently, the EXTI driver relies on dynamic runtime configuration via 
+ *            the initialization API (EXTI_Init). Pre-build static configurations 
+ *            can be added here if needed in the future.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
+
+
 #ifndef _EXTI_CONFIG_H_
 #define _EXTI_CONFIG_H_
 
