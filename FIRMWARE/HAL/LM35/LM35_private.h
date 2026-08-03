299f1d5d250fff308e57d6d1061e7101642e91b9
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 11:27:14 +0300

    Add documentation comments to LM35_private.h
    Added file header comments for documentation and clarity.

--- FIRMWARE/HAL/LM35/LM35_private.h
+++ FIRMWARE/HAL/LM35/LM35_private.h
@@ -1,3 +1,16 @@
+/**
+ * @file      LM35_private.h
+ * @author    Moaaz
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Private definitions for the LM35 Temperature Sensor Driver.
+ * @details   This file contains internal macros used for temperature calculations, 
+ *            such as the ADC reference voltage and resolution. These values are 
+ *            hardware-specific and should not be modified by upper layers.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
+
 #ifndef _LM35_PRIVATE_H_
 #define _LM35_PRIVATE_H_
 
