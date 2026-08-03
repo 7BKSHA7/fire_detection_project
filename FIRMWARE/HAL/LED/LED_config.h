0afdfb3643f6f35c72900a700221452c52d97f51
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 11:24:02 +0300

    Enhance LED_config.h with documentation and comments
    Added detailed documentation for LED configuration.

--- FIRMWARE/HAL/LED/LED_config.h
+++ FIRMWARE/HAL/LED/LED_config.h
@@ -1,3 +1,17 @@
+/**
+ * @file      LED_config.h
+ * @author    Rohayem
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Configuration file for the LED Driver.
+ * @details   This file contains user-configurable parameters such as the hardware 
+ *            ports and pins to which the Green, Yellow, and Red LEDs are connected.
+ *            Hardware Note: LEDs are configured in a source connection (Active High).
+ *            The LED's anode is connected to the MCU pin, and the cathode goes to ground.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
+
 #ifndef _LED_CONFIG_H_
 #define _LED_CONFIG_H_
 
