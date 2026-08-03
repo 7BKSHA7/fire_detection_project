4d4f211e2617e05fdf4e023b327d5740ee72b6e1
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 11:03:57 +0300

    Update header documentation for BUTTON_interface.h

--- FIRMWARE/HAL/BUTTON/BUTTON_interface.h
+++ FIRMWARE/HAL/BUTTON/BUTTON_interface.h
@@ -1,13 +1,14 @@
 /**
-* @file    Button_Interface.h
-* @author (developer)  
-* @author (reviewer)
-* @brief  
-* @details
-* @version
-* @date
-* @copyright Copyright (c) 2026, Gestell Company
-*/
+ * @file      Button_Interface.h
+ * @author    Youssef
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Interface of the Button Driver.
+ * @details   This file contains the function prototypes and public APIs used by 
+ *            upper layers to initialize and read the state of the Button module.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
 
 #ifndef BUTTON_INTERFACE_H
 #define BUTTON_INTERFACE_H
