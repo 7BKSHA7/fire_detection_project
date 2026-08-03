eb2f44e463ee87ab0f9744bec520d29fa7f444cb
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 21:09:29 +0300

    Revise header comments in EventLogger_Config.h
    Updated file header comments with author, brief, details, version, date, and copyright information.

--- FIRMWARE/APP/Safty_manager/EventLogger/EventLogger_Config.h
+++ FIRMWARE/APP/Safty_manager/EventLogger/EventLogger_Config.h
@@ -1,13 +1,17 @@
 /**
-* @file    EventLogger_Config.h
-* @author (developer)  
-* @author (reviewer)
-* @brief  
-* @details
-* @version
-* @date
-* @copyright Copyright (c) 2026, Gestell Company
-*/
+ * @file      EventLogger_Config.h
+ * @author    Beshoy
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Configuration file for the Event Logger subsystem.
+ * @details   This file is reserved for user-configurable parameters related to 
+ *            the event logging system, such as enabling/disabling logs globally, 
+ *            or changing default log messages. Currently empty but maintained 
+ *            for architectural consistency.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
+
 
 #ifndef EVENTLOGGER_CONFIG_H
 #define EVENTLOGGER_CONFIG_H
