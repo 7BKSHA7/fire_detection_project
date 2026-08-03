146819ce22e811f0cbe4df7a3333a071a2ac41fd
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 21:07:19 +0300

    Update EventLogger_Private.h

--- FIRMWARE/APP/Safty_manager/EventLogger/EventLogger_Private.h
+++ FIRMWARE/APP/Safty_manager/EventLogger/EventLogger_Private.h
@@ -1,13 +1,16 @@
 /**
-* @file    EventLogger_Private.h
-* @author (developer)  
-* @author (reviewer)
-* @brief  
-* @details
-* @version
-* @date
-* @copyright Copyright (c) 2026, Gestell Company
-*/
+ * @file      EventLogger_Private.h
+ * @author    Beshoy
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Private definitions for the Event Logger subsystem.
+ * @details   This file is reserved for internal macros, private constants, 
+ *            or specific serial framing configurations used exclusively by 
+ *            the EventLogger module. Currently empty but maintained for 
+ *            architectural consistency.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
 
 #ifndef EVENTLOGGER_PRIVATE_H
 #define EVENTLOGGER_PRIVATE_H
