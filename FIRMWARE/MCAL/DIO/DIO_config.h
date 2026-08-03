57a21fc3d78693fd91abe5cc82dc7420ee74e88e
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 17:51:59 +0300

    Add documentation comments to DIO_config.h
    Added header comments for documentation and versioning.

--- FIRMWARE/MCAL/DIO/DIO_config.h
+++ FIRMWARE/MCAL/DIO/DIO_config.h
@@ -1,3 +1,18 @@
+/**
+ * @file      DIO_config.h
+ * @author    Beshoy
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Configuration file for the DIO Driver.
+ * @details   This file is reserved for user-configurable parameters for the DIO module.
+ *            Currently, the DIO driver relies on dynamic runtime configuration via 
+ *            the control APIs. Pre-build configurations (such as initial pin directions 
+ *            or pull-up resistor settings) can be added here if needed in the future.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
+
+
 #ifndef _DIO_CONFIG_H_
 #define _DIO_CONFIG_H_
 
