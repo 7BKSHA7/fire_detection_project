322be12f7419f20673d140a22d2e6bc7691e224a
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 10:57:30 +0300

    Update documentation in Buzzer_Private.h

--- FIRMWARE/HAL/BUZZER/BUZZER_private.h
+++ FIRMWARE/HAL/BUZZER/BUZZER_private.h
@@ -1,13 +1,14 @@
 /**
-* @file    Buzzer_Private.h
-* @author (developer)  
-* @author (reviewer)
-* @brief  
-* @details
-* @version
-* @date
-* @copyright Copyright (c) 2026, Gestell Company
-*/
+ * @file      Buzzer_Private.h
+ * @author    Youssef
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Private definitions for the Buzzer Driver.
+ * @details   This file contains private macros and internal states used exclusively 
+ *            by the Buzzer driver implementation. It should not be included by upper layers.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
 
 #ifndef BUZZER_PRIVATE_H
 #define BUZZER_PRIVATE_H
