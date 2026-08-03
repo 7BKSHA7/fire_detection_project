858e121c6e665217d6bf14c2897b9a546febebb6
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 11:15:35 +0300

    Add documentation to LCD_interface.h
    Added detailed documentation for the LCD interface header file, including author information and function descriptions.

--- FIRMWARE/HAL/LCD/LCD_interface.h
+++ FIRMWARE/HAL/LCD/LCD_interface.h
@@ -1,3 +1,15 @@
+/**
+ * @file      LCD_interface.h
+ * @author    Rohayem
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Interface of the LCD (Liquid Crystal Display) Driver.
+ * @details   This file contains the function prototypes and public APIs used by 
+ *            upper layers to initialize and interact with the LCD module.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
+
 #ifndef _LCD_INTERFACE_H_
 #define _LCD_INTERFACE_H_
 
