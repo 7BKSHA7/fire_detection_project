6fdf4bb4d6d27578da2822189da61671c5506128
Youssef Medhat <ywsffyfy19@gmail.com>
2026-08-03 18:13:27 +0300

    Update TIMER_interface.h

--- FIRMWARE/MCAL/TIMER/TIMER_interface.h
+++ FIRMWARE/MCAL/TIMER/TIMER_interface.h
@@ -1,3 +1,17 @@
+/**
+ * @file      TIMER_interface.h
+ * @author    Beshoy
+ * @author    Hesham Ahmed (Reviewer)
+ * @brief     Interface of the TIMER0 Driver.
+ * @details   This file contains the function prototypes and public APIs used by 
+ *            upper layers to initialize, configure, start, and stop Timer0, 
+ *            as well as assigning callback functions for its interrupts.
+ * @version   1.0.0
+ * @date      3 August 2026
+ * @copyright Copyright (c) 2026, Gestell Company
+ */
+
+
 #ifndef _TIMER_INTERFACE_H_
 #define _TIMER_INTERFACE_H_
 
